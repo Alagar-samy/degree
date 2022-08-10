eead8683c4212f2400d1f0768da55c474f76c9e4
Alagar-samy <110388626+Alagar-samy@users.noreply.github.com>
2022-08-10 16:21:21 +0530

    Add files via upload

---
+++ degree.py
@@ -0,0 +1,25 @@
+marks=[]
+n=5
+count1=0
+count2=0
+count3=0
+count4=0
+for i in range(0,n):
+    marks.append(int(input("enter the marks:")))
+for i in range(0,n):
+    if(marks[i]>=90 or marks[i]>=40):
+        if(marks[i]>=90):
+            count1+=1
+        else:
+            count2+=1
+    elif(marks[i]>=75 or marks[i]>=50):
+        if(marks[i]>=75): 
+            count3+=1
+        else:
+            count4+=1
+if(count1==3 and count2==2 or count1==2 and count2==3):
+    print("pass")
+elif(count3==3 and count4==2 or count3==2 and count4==3):
+    print("pass")
+else:
+    print("fail")
